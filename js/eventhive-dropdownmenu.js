b4ff20d8fa2b862271e4fa7c3f72c887d101003c
vincemsnts12 <santosmerick9@gmail.com>
2025-12-01 23:45:45 +0800

    Updated add event procedure

--- js/eventhive-dropdownmenu.js
+++ js/eventhive-dropdownmenu.js
@@ -4,7 +4,7 @@ const guestLinks = document.getElementById('guestLinks');
 const userLinks = document.getElementById('userLinks');
 
 // Change to true to simulate logged-in view
-let isLoggedIn = true; 
+let isLoggedIn = false; 
 
 profileIcon.addEventListener('click', (e) => {
   e.preventDefault();
